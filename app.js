70b0e8aedb595512fb065efa69313f52b324ae13
Mark Madonna <awesomark@Marks-MacBook-Pro.local>
2026-06-07 21:50:50 -0400

    Show traditional pitcher stats with rates

--- app.js
+++ app.js
@@ -481,7 +481,7 @@ el.newGameButton.addEventListener("click", reset);
 
 async function init() {
   render();
-  const response = await fetch("./data/players.json?v=23");
+  const response = await fetch("./data/players.json?v=24");
   const data = await response.json();
   teams = data.teams;
   eras = data.eras;
