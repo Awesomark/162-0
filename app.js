146090363123610e9ff7415642d56b1f6dff51c9
Mark Madonna <awesomark@Marks-MacBook-Pro.local>
2026-07-29 17:21:22 -0400

    Add all MLB franchises

--- app.js
+++ app.js
@@ -518,7 +518,7 @@ el.newGameButton.addEventListener("click", reset);
 
 async function init() {
   render();
-  const response = await fetch("./data/players.json?v=34");
+  const response = await fetch("./data/players.json?v=35");
   const data = await response.json();
   teams = data.teams;
   eras = data.eras;
