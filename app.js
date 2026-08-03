6584101b28cd806ee44a945f4f2c9b561b6b6a22
Mark Madonna <awesomark@Marks-MacBook-Pro.local>
2026-08-02 22:42:26 -0400

    Relax relief pitcher innings eligibility

--- app.js
+++ app.js
@@ -854,7 +854,7 @@ el.newGameButton.addEventListener("click", reset);
 
 async function init() {
   render();
-  const response = await fetch("./data/players.json?v=53");
+  const response = await fetch("./data/players.json?v=54");
   const data = await response.json();
   teams = data.teams;
   eras = data.eras;
